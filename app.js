01c530436ab74ab33bf63a4f2b09ebdfcc5524a1
Ikumapayi Khalilullah Olaniyi <ikumapayiolaniyi007@gmail.com>
2022-09-15 01:17:14 +0100

    feat: create the server
    - Created the server with express,
    - Set it to listen on port 3000,
    - Imported other modules and used them accordingly for the API

--- app.js
+++ app.js
@@ -1,1 +1,48 @@
-console.log('04 Store API')
+require("dotenv").config();
+require('express-async-errors');
+
+//=========== IMPORTS ==========
+
+//----------- db ------------
+const connectDB = require("./db/connect");
+
+//---------- express ----------
+const express = require("express");
+const app = express();
+
+//---------- product route ----------
+const productsRoute = require("./routes/products");
+
+//---------- custom middleware import ---------
+const notFoundMiddleware = require("./middleware/not-found");
+const errorHandlerMiddleware = require("./middleware/error-handler");
+
+//express middleware use statements
+app.use(express.json());
+
+//======= ROUTES =======
+app.use("/api/v1/products", productsRoute);
+
+app.get("/", (req, res) => {
+  res.send("Home page");
+});
+
+//=========== CUSTOM MIDDLEWARES USE =========
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
+
+const port = process.env.PORT || 3000;
+
+const start = async () => {
+  try {
+    //await connect db
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, () => {
+      console.log(`server listening on port 3000`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
